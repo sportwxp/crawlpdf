c79c5e107214b7d6045cb76cd36df54e80e7323a
xpwang <13260487966@163.com>
2021-04-30 07:26:10 +0800

    4月30日,version2:use go routine && io function remeber close

--- main.go
+++ main.go
@@ -8,11 +8,18 @@ import (
 var (
 	// URL     string = "https://sjceodisha.in/wp-content/uploads/2019/09/"
 	// URL string = "http://index-of.es/z0ro-Repository-2/Cyber/01%20-%20Computer%20Science/"
-	URL string = "http://web.cecs.pdx.edu/~harry/cs201/slides/"
+	// URL string = "http://index-of.es/z0ro-Repository-2/Cyber/02%20-%20Electronic%20Engineering/"
+	// URL string = "http://index-of.es/z0ro-Repository-2/Cyber/03%20-%20General%20Science/"
+	// URL string = "http://index-of.es/z0ro-Repository-2/Cyber/03%20-%20General%20Science/How%20It%20Works/"
+	// URL string = "http://index-of.es/z0ro-Repository-2/Cyber/04%20-%20Fiction/WSB/"
+
+	// URL string = "http://index-of.es/z0ro-Repository-2/Cyber/05%20-%20Military%20&%20Espionage/"
+	URL string = "http://index-of.es/z0ro-Repository-2/Cyber/06%20-%20Miscellaneous/"
+	// URL string = "http://web.cecs.pdx.edu/~harry/cs201/slides/"
 	// PATTERN string = `.+href=\"(.+\.pdf)\">`
 	PATTERN string = `.+href=\"(.+\.pdf)\">`
-	splite  string = "<tr>"
-	// splite   string = "\n"
+	// splite  string = "<tr>"
+	splite   string = "\n"
 	savepath string = "./books"
 )
 
--- urlbooks/mymmapwrite.go
+++ urlbooks/mymmapwrite.go
@@ -5,7 +5,9 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os/exec"
+	"net/url"
+	"os"
+	"path"
 	"regexp"
 	"strings"
 	"sync"
@@ -40,8 +42,8 @@ func (b *BookList) GetParse() error {
 	if err != nil {
 		return err
 	}
-	// fmt.Print(string(content))
-	// os.WriteFile("book.html", content, 0666)
+	fmt.Print(string(content))
+	os.WriteFile("book.html", content, 0666)
 	// log.Fatal("stop")
 	for _, line := range strings.Split(string(content), b.Split) {
 		tokens := b.StringPattern.FindStringSubmatch(line)
@@ -50,7 +52,9 @@ func (b *BookList) GetParse() error {
 			// fmt.Println(b.BookList[len(b.BookList)-1])
 		}
 	}
-	log.Print(b.BookList)
+	for _, name := range b.BookList {
+		log.Print(name)
+	}
 	return nil
 }
 
@@ -66,30 +70,74 @@ func (b *BookList) GetFile(savepath string) error {
 		// defer response.Body.Close()
 
 		// bytecontent, _ := io.ReadAll(response.Body)
-		// fmt.Printf("read %d bytes in memroy\n", len(bytecontent))
+		// // fmt.Printf("read %d bytes in memroy\n", len(bytecontent))
 		// savefilename := path.Join(savepath, strings.Split(href, "/")[len(strings.Split(href, "/"))-1])
-		// err = os.WriteFile(savefilename, bytecontent, 0666)
+		// f, err := os.OpenFile(savefilename, os.O_RDWR|os.O_CREATE, 0755)
+		// if err != nil {
+		// 	log.Fatal("os open file wrong")
+		// }
+		// defer f.Close()
+		// n, err := f.Write(bytecontent)
+		// if err != nil {
+		// 	log.Fatal("os open file wrong")
+		// }
+		// if n != len(bytecontent) {
+		// 	panic("write length err")
+		// }
 		// if err != nil {
 		// 	return err
 		// }
 		// fmt.Printf("get %s done\n", savefilename)
 		wg.Add(1)
 
-		go wgetfile(href, savepath, wg)
+		go routine_get(href, savepath, wg)
 	}
 	wg.Wait()
 
 	return nil
 }
 
-func wgetfile(name, savepath string, wg *sync.WaitGroup) {
+// func wgetfile(name, savepath string, wg *sync.WaitGroup) {
 
-	fmt.Printf("Start ***** %s begin \n", name)
-	cmd := exec.Command("wget", name)
-	cmd.Dir = savepath
-	// log.Print(savepath)
-	err := cmd.Run()
-	log.Printf("???Command finished with error: %v", err)
-	fmt.Printf("Down %%%%%% %s done \n", name)
+// 	fmt.Printf("Start ***** %s begin \n", name)
+// 	cmd := exec.Command("wget", name)
+// 	cmd.Dir = savepath
+// 	// log.Print(savepath)
+// 	err := cmd.Run()
+// 	log.Printf("???Command finished with error: %v", err)
+// 	fmt.Printf("Down %%%%%% %s done \n", name)
+// 	wg.Done()
+// }
+
+func routine_get(href, savepath string, wg *sync.WaitGroup) {
+	response, err := http.Get(href)
+	if err != nil {
+		log.Fatal("http get file wrong")
+	}
+	fmt.Println("http get success")
+	defer response.Body.Close()
+
+	bytecontent, _ := io.ReadAll(response.Body)
+	// fmt.Printf("read %d bytes in memroy\n", len(bytecontent))
+	savefilename := path.Join(savepath, strings.Split(href, "/")[len(strings.Split(href, "/"))-1])
+	log.Printf("%s download begin", savefilename)
+	savefilename, err = url.PathUnescape(savefilename)
+	if err != nil {
+		log.Fatal("unescape erro")
+	}
+	f, err := os.OpenFile(savefilename, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal("os open file wrong")
+	}
+	defer f.Close()
+	n, err := f.Write(bytecontent)
+	if err != nil {
+		log.Fatal("os open file wrong")
+	}
+	if n != len(bytecontent) {
+		panic("write length err")
+	}
+	log.Printf("%s download done", savefilename)
 	wg.Done()
+
 }
